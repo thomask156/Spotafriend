f7b487a83e5844724639c1c8f76403fec4fd2dc1
Thomask156 <Thomask156@gmail.com>
2021-03-28 11:38:41 -0400

    Comming express js backedn with JWT Auth, PG integration

---
+++ spotafriend-backend/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "thomas-secret-key"
+}
---
+++ spotafriend-backend/config/db.config.js
@@ -0,0 +1,16 @@
+const { decodeBase64 } = require("bcryptjs");
+
+module.exports = {
+    HOST: "localhost",
+    USER: "postgres",
+    PASSWORD: "postgres",
+    DB: "testdb",
+    dialect: "postgres",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+
+}
